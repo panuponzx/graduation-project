b2a81903c7b4c324dd3728561c744d51ec79debc
bas <baspanupon84862@gmail.com>
2025-01-29 15:47:05 +0700

    start-project-DS

---
+++ src/main/java/com/graduation_project/digital_signature/utils/KeyUtils.java
@@ -0,0 +1,4 @@
+package com.graduation_project.digital_signature.utils;
+
+public class KeyUtils {
+}
---
+++ src/test/java/com/graduation_project/digital_signature/DigitalSignatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation_project.digital_signature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSignatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
