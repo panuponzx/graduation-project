0fa10fb7b713d3381787f09ed5dcd6188642b229
bas <baspanupon84862@gmail.com>
2025-01-29 10:48:36 +0700

    start project

---
+++ src/main/java/com/graduation_project/digital_signature/DigitalSignatureApplication.java
@@ -0,0 +1,13 @@
+package com.graduation_project.digital_signature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureApplication.class, args);
+	}
+
+}
